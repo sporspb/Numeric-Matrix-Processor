a380d6ad776c935e2ba380cef238d432bc4b2df5
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-12-16 18:04:56 +0300

    completed Currying

---
+++ Problems/Reducer operator/src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.function.IntBinaryOperator;
+
+class CustomReducer {
+
+    /**
+     * The operator combines all values in the given range into one value
+     * using combiner and initial value (seed)
+     */
+    public static final BiFunction<Integer, IntBinaryOperator, IntBinaryOperator> reduceIntOperator =
+
+            (initValue, combiner) -> (left, right) -> {
+                int value = initValue;
+                for (int i = left; i <= right; i++) {
+                    value = combiner.applyAsInt(value, i);
+                }
+                return value;
+            };
+
+
+    /**
+     * The operator calculates the sum in the given range (inclusively)
+     */
+    public static final IntBinaryOperator sumOperator = reduceIntOperator.apply(0, (x, y) -> x + y);
+
+    /**
+     * The operator calculates the product in the given range (inclusively)
+     */
+    public static final IntBinaryOperator productOperator =
+            (left, right) -> {
+                int value = 1;
+                for (int i = left; i <= right; i++) {
+                    value *= i;
+                }
+                return value;
+            };
+
+    // Don't change the code below
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] values = scanner.nextLine().split(" ");
+
+        int l = Integer.parseInt(values[0]);
+        int r = Integer.parseInt(values[1]);
+
+        int sumReducer = reduceIntOperator.apply(0, Integer::sum).applyAsInt(l, r);
+        int sum = sumOperator.applyAsInt(l, r);
+
+        int prodReducer = reduceIntOperator.apply(1, (x, y) -> x * y).applyAsInt(l, r);
+        int prod = productOperator.applyAsInt(l, r);
+
+        System.out.println(String.format("%d %d %d %d", sumReducer, sum, prodReducer, prod));
+    }
+}
