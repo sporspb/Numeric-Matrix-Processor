f36cb7850bee2aceed2b54b5e2c45894bc142dfe
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-12-17 11:50:44 +0300

    completed completed Stream filtering

---
+++ Problems/Check if a number is prime/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.stream.LongStream;
+
+class PrimeNumbers {
+
+    /**
+     * Checking if a number is prime
+     *
+     * @param number to test >= 2
+     * @return true if number is prime else false
+     */
+    private static boolean isPrime(long number) {
+        final long count = LongStream.rangeClosed(2, (long) Math.sqrt(number)).filter(l -> number % l == 0).count();
+        return count == 0;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine().trim();
+
+        int n = Integer.parseInt(line);
+
+        System.out.println(isPrime(n) ? "True" : "False");
+    }
+}
