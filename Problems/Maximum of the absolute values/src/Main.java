ffef511878e0462e1368ffdfeed4a64029c1f25a
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-12-17 15:19:14 +0300

    completed Map and flatMap

---
+++ Problems/Maximum of the absolute values/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * Returns the largest absolute value in the array of numbers.
+     *
+     * @param numbers the input array of String integer numbers
+     * @return the maximum integer absolute value in the array
+     */
+    public static int maxAbsValue(String[] numbers) {
+        return Arrays.stream(numbers)
+                .map(Integer::parseInt)
+                .map(Math::abs)
+                .max(Integer::compareTo).orElse(0);
+    }
+
+    // Don't change the code below
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(maxAbsValue(scanner.nextLine().split("\\s+")));
+    }
+}
